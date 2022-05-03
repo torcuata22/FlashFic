291f94552790c17e2f211dff3ae7ae8be1425daa
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-03 16:54:17 -0400

    testing dynamic urls

--- flash/challenges/urls.py
+++ flash/challenges/urls.py
@@ -3,16 +3,18 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('january', views.first),
-    path('february', views.second),
-    path('march', views.third),
-    path('april', views.fourth),
-    path('may', views.fifth),
-    path('june', views.sixth),
-    path('july', views.seventh),
-    path('august', views.eighth),
-    path('september', views.ninth),
-     path('october', views.tenth),
-    path('november', views.eleventh),
-    path('december', views.twelfth)
+    path("<int:month>", views.challenge_by_number),
+    path("<str:month>", views.monthly_challenge)
+    # path('january', views.first),
+    # path('february', views.second),
+    # path('march', views.third),
+    # path('april', views.fourth),
+    # path('may', views.fifth),
+    # path('june', views.sixth),
+    # path('july', views.seventh),
+    # path('august', views.eighth),
+    # path('september', views.ninth),
+    #  path('october', views.tenth),
+    # path('november', views.eleventh),
+    # path('december', views.twelfth)
 ]
--- flash/challenges/views.py
+++ flash/challenges/views.py
@@ -1,45 +1,63 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("A year of writing exercises")
 
-def first(request):
-    return HttpResponse("Writing exercise for January!")
+def challenge_by_number(request, month):
+    return HttpResponse(month)
+    
 
-def second(request):
-    return HttpResponse("Writing exercise for February!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'Writing exercise for January'
+    elif month == 'february':
+        challenge_text='Writing exercise for February'
+    elif month == 'march':
+        challenge_text='Writing exercise for March'
+    else:
+        return HttpResponseNotFound ('This month is not supported')
+    
+    return HttpResponse(challenge_text)
+    
 
-def third(request):
-    return HttpResponse("Writing exercise for March!")
+# def first(request):
+#     return HttpResponse("Writing exercise for January!")
 
-def fourth(request):
-    return HttpResponse("Writing exercise for April!")
+# def second(request):
+#     return HttpResponse("Writing exercise for February!")
 
-def fifth(request):
-    return HttpResponse("Writing exercise for May!")
+# def third(request):
+#     return HttpResponse("Writing exercise for March!")
 
-def sixth(request):
-    return HttpResponse("Writing exercise for June!")
+# def fourth(request):
+#     return HttpResponse("Writing exercise for April!")
 
-def seventh(request):
-    return HttpResponse("Writing exercise for July!")
+# def fifth(request):
+#     return HttpResponse("Writing exercise for May!")
 
-def eighth(request):
-    return HttpResponse("Writing exercise for August!")
+# def sixth(request):
+#     return HttpResponse("Writing exercise for June!")
 
-def ninth(request):
-    return HttpResponse("Writing exercise for September!")
+# def seventh(request):
+#     return HttpResponse("Writing exercise for July!")
 
-def tenth(request):
-    return HttpResponse("Writing exercise for October!")
+# def eighth (request):
+#     return HttpResponse("Writing exercise for August!")
 
-def eleventh(request):
-    return HttpResponse("Writing exercise for November!")
+# def ninth(request):
+#     return HttpResponse("Writing exercise for September!")
 
-def twelfth(request):
-    return HttpResponse("Writing exercise for December!")
+# def tenth(request):
+#     return HttpResponse("Writing exercise for October!")
+
+# def eleventh(request):
+#     return HttpResponse("Writing exercise for November!")
+
+# def twelfth(request):
+#     return HttpResponse("Writing exercise for December!")
 
 
