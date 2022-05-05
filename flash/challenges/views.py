d05209504fc3585679f4fc087545a219b8ad4dfe
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2022-05-05 15:15:13 -0400

    started templates

--- flash/challenges/views.py
+++ flash/challenges/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
+
 monthly_challenges =  {
     "january": "Writing exercises for January",
     "february": "Writing exercise for Ferbuary",
@@ -13,22 +15,18 @@ monthly_challenges =  {
     "september":"Writing exercise for September",
     "october":"Writing exercise for October",
     "november":"Writing exercise for November", 
-    "december":"Writing exercise for December"
+    "december": None
     
 }
 
 # Create your views here.
 
 def index(request): 
-    list_items = ""
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href = \"{month_path}\"> {capitalized_month}</a></li>"
-    #side-by-side list items, now we wrap them in unordered list:
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    
+    return render(request, "challenges/index.html", {
+        "months": months
+    }) 
 
 def challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -44,8 +42,10 @@ def challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render (request,"challenges/challenge.html", {
+            "text": challenge_text , 
+            "month_name": month   
+        })
     except:
         return HttpResponseNotFound ('<h1>This month is not supported</h1>')
     
